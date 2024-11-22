97f235ac930c2edc48483ea8e5ab9c96fe086c86
ArielGnz <arielgon2206@gmail.com>
2024-11-22 10:18:29 -0300

    empezando el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Proyecto de single page de la Doctora CG</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
