045935f51e0bd3b30b746101f9cd1d7428cb891d
ArielGnz <arielgon2206@gmail.com>
2024-11-27 20:34:33 -0300

    sigo en el nav

--- src/Header/NavBar.jsx
+++ src/Header/NavBar.jsx
@@ -6,7 +6,7 @@ import { IoMdClose } from "react-icons/io";
 
 import { FiUser } from "react-icons/fi";
 import { RiShutDownLine, RiVideoUploadLine } from "react-icons/ri";
-import logo from '../assets/Dr.png'
+import logo from "../assets/Dr.png";
 
 export const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -19,8 +19,22 @@ export const NavBar = () => {
   };
 
   return (
-    <nav className="bg-white shadow-lg w-full flex  justify-between items-center mx-auto py-6 md:px-8 ">
-      <img src={logo} alt="" className="w-[150px] border-2 border-red-500" />
+    <nav className="bg-white shadow-lg w-full flex justify-between items-center mx-auto py-4 md:px-8 ">
+      <div>
+        <img src={logo} alt="" className="w-[140px] border-2 border-red-500" />
+      </div>
+
+      <div>
+        <ul className="flex justify-center items-center">
+          <li className="mx-10">Inicio</li>
+          <li className="flex flex-row mx-10">Sobre Mi</li>
+          <li className="mx-10">Experiencia</li>
+        </ul>
+      </div>
+
+      <div>
+        <button className=""></button>
+      </div>
       <button
         onClick={toggleMenu}
         className="text-2xl md:hidden block text-slate-800 me-2 focus:outline-none"
@@ -32,18 +46,12 @@ export const NavBar = () => {
           menuOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
         } md:opacity-100 md:max-h-full md:flex-grow overflow-visible`}
       >
-        <div className="flex-grow flex justify-center mt-2  w-full md:w-auto">
-          
-        </div>
+        <div className="flex-grow flex justify-center mt-2  w-full md:w-auto"></div>
         <div className="relative flex flex-col md:flex-row items-center me-3 md:space-x-2 mt-4 md:mt-0">
           <div
             onClick={toggleDropdown}
             className="flex items-center cursor-pointer space-x-1"
-          >
-            
-  
-            
-          </div>
+          ></div>
           {dropdownOpen && (
             <div className="absolute top-full right-0 bg-white shadow-md rounded-lg w-48 py-2 mt-2 z-50 text-sm">
               <a
@@ -70,7 +78,6 @@ export const NavBar = () => {
             </div>
           )}
         </div>
-
       </div>
     </nav>
   );
