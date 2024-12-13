3fc702e81b5595cc690c9022b857b8b4d892fc27
ArielGnz <arielgon2206@gmail.com>
2024-12-13 20:24:17 -0300

    working

--- src/components/SobreMi.jsx/SobreMi.jsx
+++ src/components/SobreMi.jsx/SobreMi.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import images from "../../assets/images.jpg";
 
-
 export const SobreMi = () => {
-  
   const [view, setView] = useState("sobreMi");
-  
+
   const handleView = (option) => {
     setView(option);
   };
@@ -14,13 +12,28 @@ export const SobreMi = () => {
     <div className="flex flex-col bg-red-900">
       <div className="hidden mt-10 md:flex justify-center text-2xl font-semibold text-gray-600 ">
         <div className="w-[120px] mx-12 border-b-4 hover:text-sky-500 hover:border-sky-500 cursor-pointer">
-          <span className="flex justify-center" onClick={() => handleView("sobreMi") }>Sobre Mi</span>
+          <span
+            className="flex justify-center"
+            onClick={() => handleView("sobreMi")}
+          >
+            Sobre Mi
+          </span>
         </div>
         <div className="w-[150px] mx-12 border-b-4 hover:text-sky-500 hover:border-sky-500 cursor-pointer">
-          <span className="flex justify-center" onClick={() => handleView("experiencia")} >Experiencia</span>
+          <span
+            className="flex justify-center"
+            onClick={() => handleView("experiencia")}
+          >
+            Experiencia
+          </span>
         </div>
         <div className="w-[180px] mx-12 border-b-4 hover:text-sky-500 hover:border-sky-500 cursor-pointer">
-          <span className="flex justify-center" onClick={() => handleView("especialidades")} >Especialidades</span>
+          <span
+            className="flex justify-center"
+            onClick={() => handleView("especialidades")}
+          >
+            Especialidades
+          </span>
         </div>
       </div>
 
@@ -29,6 +42,14 @@ export const SobreMi = () => {
           <img src={images} className="mx-auto h-[380px] w-[380px]" />
         </div>
         <div className="hidden md:block md:w-2/3 ">
+          <div>
+            <p className="mx-2 font-semibold text-gray-600 text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+              asperiores distinctio suscipit cupiditate blanditiis consequuntur
+              eius odit voluptas! Earum consequuntur a aspernatur delectus
+              debitis laboriosam inventore dolorem, optio facere quam!
+            </p>
+          </div>
           <p className="mx-2 font-semibold text-gray-600 text-2xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
             asperiores distinctio suscipit cupiditate blanditiis consequuntur
